8d60e76caaf1d0fc4476f160d70375a16955d862
frankdev95 <frankdev95@protonmail.com>
2020-12-23 09:38:55 +0000

    Create the main section of home page and the settings tab, and add encryption and decryption functionality using mongoose-encryption

--- app.js
+++ app.js
@@ -2,6 +2,59 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const app = express();
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri =
+    "mongodb+srv://frank-admin:14RK68SG7nrXqQ7g@cluster0.makro.mongodb.net/test?retryWrites=true&w=majority";
+const url =
+    "mongodb://localhost:27017/userDB";
+
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {});
+
+const patterns = {
+    name: /^[a-z ,.'-]+$/i,
+    email: /^([a-z\d."+\-_]+)@([a-z\d-.[\]]+)\.([a-z\d]{2,8})(\.[a-z]+)?$/i,
+    username: /^[a-z\d]{5,12}$/i,
+    password: /^(?=.*[a-z])(?=.*[\d])(?=.*[\W])[a-z\d\W]{8,25}$/i,
+}
+
+const userSchema = new mongoose.Schema ({
+    name: {
+        type: String,
+        required: [true, 'Please provide a valid name'],
+        match: patterns.name
+    },
+    username: {
+        type: String,
+        required: [true, 'Please provide a valid username'],
+        match: patterns.username
+    },
+    email: {
+        type: String,
+        lowercase: true,
+        required: [true, 'Please provide an email address'],
+        match: patterns.email
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        match: patterns.password
+    }
+});
+
+const encKey = 'qdLi35Zp8jJWhZlxGcIPewqWJPYMb3IOijPzXjvDABg=';
+const sigKey = 'SXvtfI1yFx4hZCWmFAGNfyO4PXz7yRPNxRPlgm88LtF9z_ADktx-5MgtKuQUtbU6bstHLF17AaqgSkfZ2JYL9g';
+
+userSchema.plugin(encrypt, {encryptionKey: encKey, signingKey: sigKey, encryptedFields: ['password']});
+
+const User = new mongoose.model('User', userSchema);
 
 let port = process.env.PORT;
 if(port === null || port === undefined) {
@@ -17,13 +70,70 @@ app.listen(port, () => {
 });
 
 app.get('/login', (req, res) => {
-    res.render('login');
+    res.render('login', {
+        confirmationMessage: null,
+        usernamePlaceholder: null
+    });
 });
 
 app.get('/', (req, res) => {
-    res.render('login');
+    res.redirect('/login');
 });
 
-app.get("/register", (req, res) => {
+app.get('/register', (req, res) => {
     res.render('registration');
-})
+});
+
+app.get('/home', (req, res) => {
+    res.render('home');
+});
+
+app.post('/register', (req, res) => {
+    const newUser = new User({
+        name: req.body.name,
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    newUser.save((err) => {
+        if(err) {
+            console.log(err);
+        } else {
+            let confirmationMessage = 'Registration successful, please login using credentials.'
+            res.render('login', {
+                confirmationMessage: confirmationMessage,
+                usernamePlaceholder: req.body.username
+            });
+        }
+    });
+});
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({username: username},function(err, user) {
+        if(err) {
+            console.log(err);
+        } else {
+            if(user === null) {
+                let confirmationMessage = 'Username does not exist, please enter a valid username.';
+                res.render('login', {
+                    confirmationMessage: confirmationMessage,
+                    usernamePlaceholder: null
+                });
+            } else {
+               if(user.password === password) {
+                   res.render('home');
+               } else {
+                   let confirmationMessage = 'Password does not match username, please enter the correct password.';
+                   res.render('login', {
+                       confirmationMessage: confirmationMessage,
+                       usernamePlaceholder: username
+                   })
+               }
+           }
+        }}
+    )
+});
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,6 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    /* NAV VARIABLES*/
+    /* REGISTER / LOGIN PAGE VARIABLES */
+
+    let formGroups = document.querySelectorAll('.register__form .form__group:not(:last-child)');
+    let loginMessage = document.querySelector('.login .message');
+    let loginMessageText = document.querySelector('.login .message p');
+    let loginMessageClose = document.querySelector('.login .message svg');
+
+    /* NAV VARIABLES */
 
     let navDropdownBtn = document.querySelector(".dropdown__btn-item");
     let navDropdownContent = document.querySelector(".dropdown__content");
@@ -11,10 +18,75 @@ document.addEventListener("DOMContentLoaded", function() {
 
     let cards = document.querySelectorAll(".organiser__item")
 
-    navDropdownBtn.addEventListener("click", toggleDropdown);
+    /* REGEX PATTERNS */
 
-    for(let i = cards.length -1; i >= 0; i--) {
-        cards[i].style.width = 200 + i * 50 + "px";
+    const patterns = {
+        name: /^[a-z ,.'-]+$/i,
+        email: /^([a-z\d."+\-_]+)@([a-z\d-.[\]]+)\.([a-z\d]{2,8})(\.[a-z]+)?$/i,
+        username: /^[a-z\d]{5,12}$/i,
+        password: /^(?=.*[a-z])(?=.*[\d])(?=.*[\W])[a-z\d\W]{8,25}$/i,
+    }
+
+    /* REGISTER PAGE TOOLTIP */
+
+    formGroups.forEach((group) => {
+        let setProperty;
+        const input = group.querySelector('input');
+        const tooltip = group.querySelector('.tooltip');
+
+        group.addEventListener('mouseover', () => {
+            setProperty = setTimeout(() =>   tooltip.style.setProperty('display', 'initial', 'important'), 700);
+        });
+
+        group.addEventListener('mouseout', () => {
+            clearTimeout(setProperty);
+            tooltip.style.setProperty('display', 'none', 'important');
+        });
+
+        input.addEventListener('focus', () => {
+            clearTimeout(setProperty);
+            tooltip.style.setProperty('display', 'none', 'important');
+        });
+
+        input.addEventListener('keyup', registerValidate);
+    });
+
+    function registerValidate() {
+        const regex = patterns[this.name];
+        const value = this.value;
+
+        if(regex.test(value)) {
+            this.style.border = '2px solid #28df99';
+        } else {
+            this.style.border = '2px solid #ff9642';
+        }
+
+        if(value === '') {
+            this.style.border = '1px solid #868686';
+        }
+    }
+
+    /* LOGIN PAGE MESSAGE */
+
+    if(document.body.contains(loginMessage)) {
+        if(loginMessageText.textContent.length > 0) {
+            loginMessage.style.opacity = "1";
+            loginMessageClose.style.pointerEvents = "initial";
+        } else {
+            loginMessage.style.opacity = "0";
+            loginMessageClose.style.pointerEvents = "none";
+        }
+
+        loginMessageClose.addEventListener('click', () => {
+            loginMessage.style.opacity = "0";
+            loginMessageClose.style.pointerEvents = "none";
+        });
+    }
+
+    /* HOME PAGE NAV DROPDOWN */
+
+    if(document.body.contains(navDropdownBtn)) {
+        navDropdownBtn.addEventListener("click", toggleDropdown);
     }
 
     function toggleDropdown() {
@@ -23,12 +95,5 @@ document.addEventListener("DOMContentLoaded", function() {
         navDropdownList.classList.add("animated");
     }
 
-    function slipCard(card) {
-        card.style.width = "300px";
-    }
+    /* HOME PAGE */
 })
-
-class Card {
-    constructor(name, description, borderCol) {
-    }
-}
